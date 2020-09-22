1e849ba5458687cc6db9603437d07ce932e1a251
Joseph <joseph300905@gmail.com>
2020-09-22 23:43:07 +0200

    Update App component

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Video from './Video';
+
+class App extends React.Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+            videos: [],
+            query: '',
+        };
+        this.load = this.load.bind(this);
+    }
+
+    load () {
+        axios.post('api/search/', {
+            query: this.state.query,
+        }, {
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        })
+            .then(res => this.setState({
+                videos: res.data.videos,
+            }))
+            .catch(err => console.error(err.response));
+    }
+
+    render () {
+        return (
+            <div className="App">
+                <div className="Search">
+                    <h1>YTDL</h1>
+                    <div className="Bar">
+                        <input type="text" onChange={evt => this.setState({ query: evt.target.value })}></input>
+                        <button onClick={() => this.load()} className="Load">Search</button>
+                    </div>
+                </div>
+                <div className="Videos">
+                    {this.state.videos.map(video => <Video title={video.title} author={video.author.name} url={video.url} image={video.image} id={video.videoId} key={video.videoId} />)}
+                </div>
+            </div>
+        )
+    }
 }
 
 export default App;
