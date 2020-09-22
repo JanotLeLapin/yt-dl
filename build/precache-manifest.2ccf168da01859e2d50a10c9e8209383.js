af89adef8025c90af7d9a8cb203a083bf4210d4b
Joseph <joseph300905@gmail.com>
2020-09-22 23:44:43 +0200

    Build

---
+++ build/precache-manifest.2ccf168da01859e2d50a10c9e8209383.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "771ab1d5bc32f667d39bdc12ceeb5441",
+    "url": "/index.html"
+  },
+  {
+    "revision": "45723f55d90af0439555",
+    "url": "/static/css/main.45fbc607.chunk.css"
+  },
+  {
+    "revision": "8aa05ab23450f8462ae1",
+    "url": "/static/js/2.77b19981.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.77b19981.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45723f55d90af0439555",
+    "url": "/static/js/main.a02dba28.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
